c30bd09a2d2ced13b0b41b6015a191c7e1ad26de
Sharmina Akhtar <sakht318@gmail.com>
2018-08-08 20:06:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
